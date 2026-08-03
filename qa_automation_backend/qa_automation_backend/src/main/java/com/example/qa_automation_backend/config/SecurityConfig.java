4f8bcbac4d400c73df39f6d345e3f6dfa9edca99
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2026-08-03 14:58:11 +0200

    implement client management CRUD module in front end, fix on api.

--- qa_automation_backend/qa_automation_backend/src/main/java/com/example/qa_automation_backend/config/SecurityConfig.java
+++ qa_automation_backend/qa_automation_backend/src/main/java/com/example/qa_automation_backend/config/SecurityConfig.java
@@ -1,5 +1,26 @@
 package com.example.qa_automation_backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
 public class SecurityConfig {
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .cors(cors -> {})
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/**").permitAll()
+                    .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
 }
