f7a2800f8024ec811c29adea35f0491c2157798a
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2026-08-28 21:26:54 +0200

    Security config modified.

--- qa_automation_backend/qa_automation_backend/src/main/java/com/example/qa_automation_backend/config/SecurityConfig.java
+++ qa_automation_backend/qa_automation_backend/src/main/java/com/example/qa_automation_backend/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,8 +15,8 @@ public class SecurityConfig {
             .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/**").permitAll()
-                    .anyRequest().permitAll()
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().permitAll()
             );
 
         return http.build();
